d267d8c1ef0912a3895c90836b200cd41704e1f6
cainiaoff <1509706160@qq.com>
2022-05-21 10:15:40 +0800

    首页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,9 @@
 $.ajaxPrefilter((options) => {
   options.url = `http://www.liulongbin.top:3007` + options.url;
+  //在请求之前给有权限的接口注入token
+  if (options.url.includes("/my/")) {
+    options.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
 });
